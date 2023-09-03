af75a62693ac0abdd5134356cdc138ae7cef8b07
Juliet Onyekaoha <happyjuliet@rocketmail.com>
2023-09-03 21:59:33 +0200

    added configuration to generate dynamic html files, cusrtomized generated html file, seperated config file to production and dev files, added dev-server configuration

---
+++ dist/main.44df12186edefa0e52a6.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatewebpack"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './components/heading/heading.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n/* harmony import */ var _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/hello-world-button/hello-world-button.js */ \"./src/components/hello-world-button/hello-world-button.js\");\n\n\nvar heading = new Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './components/heading/heading.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();\nheading.render();\nvar helloWorldButton = new _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"]();\nhelloWorldButton.render();\nif (false) {} else if (true) {\n  console.log(\"development mode\");\n}\nhelloWorldButton.methoddoesnotexisti();\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("04a46748787bffbbb703")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ dist/main.b219375e0e03e052aab1.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatewebpack"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/hello-world-button/hello-world-button.js */ \"./src/components/hello-world-button/hello-world-button.js\");\n\nvar heading = new Heading();\nheading.render();\nvar helloWorldButton = new _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\nhelloWorldButton.render();\nif (false) {} else if (true) {\n  console.log(\"development mode\");\n}\nhelloWorldButton.methoddoesnotexisti();\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("44df12186edefa0e52a6")
+/******/ })();
+/******/ 
+/******/ }
+);
